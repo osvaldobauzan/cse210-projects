fa197188d04f97b5bfaad33a8247c954318fd680
Osvaldo Bauzan <osvaldo.bauzan@gmail.com>
2023-11-03 21:39:13 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Time to rock");
+
+        Console.Write("What is your grade?:");
+        string initialGrade = Console.ReadLine();
+        int grade = int.Parse(initialGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine($"Your grade is A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine($"Your grade is B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine($"Your grade is C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine($"Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your gade is F");
+        }
+
+
+
+        if (grade >= 70 )
+        {
+            Console.WriteLine("Congratulations, you passed the course");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, you didn't approve the course");
+        }
+
+        
+        
     }
 }
