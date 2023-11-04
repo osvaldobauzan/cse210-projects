53026a39e9e81404f5690a2f987ead2e1b26cc6a
Osvaldo Bauzan <osvaldo.bauzan@gmail.com>
2023-11-03 21:09:24 -0600

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your name?:");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name?:");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {name} {last}.");
     }
 }
