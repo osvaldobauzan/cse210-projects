edb2fe7df92aec91c1fa570c092e8ec90f4c45a9
Osvaldo Bauzan <osvaldo.bauzan@gmail.com>
2023-11-04 17:18:55 -0600

    prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Apoco si papito");
+
+        //Console.Write("What is the magic number?");
+        //string magicStri = Console.ReadLine();
+        //int magic = int.Parse(magicStri);
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess from 1 to 100?");
+            string guessStri = Console.ReadLine();
+            guess = int.Parse(guessStri);
+            
+            if ( guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if ( guess == magic)
+            {
+                Console.WriteLine("Perfect!");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        
     }
 }
