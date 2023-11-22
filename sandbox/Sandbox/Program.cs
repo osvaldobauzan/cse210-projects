f9917527dbb5df6ee0e6991e940f7f897f83bc10
Osvaldo Bauzan <osvaldo.bauzan@gmail.com>
2023-11-22 06:49:16 -0600

    az305

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello AZ305! Date: 22 Nov 2023 6:48 am");
     }
 }
